9f9ad004dc429fc2f359cbdd38dca706de8885a1
TK-moon <tkmoon.dev@gmail.com>
2026-08-11 22:07:05 +0900

    인사말 추가

--- src/sections/GreetingSection/Messages/index.tsx
+++ src/sections/GreetingSection/Messages/index.tsx
@@ -6,10 +6,11 @@ interface Props {}
 const Messages: FC<Props> = () => {
   return (
     <div className={styles.messages}>
-      <p>에베베</p>
-      <p>에베베베</p>
-      <p>에베베베베</p>
-      <p>에베베, 에베베베</p>
+      <p>함께라서 더 즐거웠던 시간들이 모여</p>
+      <p>이제 부부가 되려 합니다.</p>
+      <p>서로를 아끼며 씩씩하게 살아갈게요.</p>
+      <p>바쁘시더라도 오셔서</p>
+      <p>저희의 새로운 시작을 지켜봐 주세요.</p>
     </div>
   );
 };
